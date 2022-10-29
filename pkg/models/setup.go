64407b5d469a267a8fb98b6bbe82edca6f36f1c3
DanArmor <danilamorozov07@mail.ru>
2022-10-29 22:36:09 +0300

    fix: debug log

--- pkg/models/setup.go
+++ pkg/models/setup.go
@@ -5,10 +5,13 @@ package models
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func ConnectDatabase(sqlurl string) *gorm.DB {
-	database, err := gorm.Open(mysql.Open(sqlurl), &gorm.Config{})
+	database, err := gorm.Open(mysql.Open(sqlurl), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 
 	if err != nil {
 		panic("Failed to connect to database!")
