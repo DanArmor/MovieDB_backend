0d888df1ae3178dd51b65f6775f6fb16d62aee4f
danila <danilamorozov07@mail.ru>
2022-11-14 01:42:28 +0300

    fix: fix

--- cmd/moviedb.go
+++ cmd/moviedb.go
@@ -124,8 +124,8 @@ func main() {
 			log.Printf("Listen: %s\n", err)
 		}
 	}()
-	quit := make(chan os.Signal)
-	signal.Notify(quit, os.Interrupt, os.Kill, syscall.SIGTERM, syscall.SIGINT)
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGUSR1)
 	<-quit
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
